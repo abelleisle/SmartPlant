e46ad5aee743d48fcb0308a8a1c7ecfc8e0631bc
Andy Belle-Isle <abelleisle@protonmail.com>
2022-11-05 00:32:17 -0500

    Created platformIO project

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#define ETL_NO_STL
+
+#include <Embedded_Template_Library.h> // Mandatory for Arduino IDE only
+#include <etl/vector.h>
+
+void setup()
+{
+    etl::vector<int, 16> vec;
+}
+
+void loop()
+{
+
+}
